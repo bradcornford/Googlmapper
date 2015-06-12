84d9e6d7dc5e0f08029cb12eb2d842cea36de5ae
Bradley Cornford <bcornford@plus.net>
2015-06-12 11:18:53 +0100

    Updated support for Laravel 5.

--- src/views/polygon.blade.php
+++ src/views/polygon.blade.php
@@ -1,17 +1,17 @@
-var polygonCoordinates_{{ $id }} = [
+var polygonCoordinates_{!! $id !!} = [
 	@foreach ($options['coordinates'] as $coordinate)
-		new google.maps.LatLng({{ $coordinate['latitude'] }}, {{ $coordinate['longitude'] }}),
+		new google.maps.LatLng({!! $coordinate['latitude'] !!}, {!! $coordinate['longitude'] !!}),
 	@endforeach
 ];
 
-var polygon_{{ $id }} = new google.maps.Polygon({
-	paths: polygonCoordinates_{{ $id }},
-	strokeColor: '{{ $options['strokeColor'] }}',
-	strokeOpacity: {{ $options['strokeOpacity'] }},
-	strokeWeight: {{ $options['strokeWeight'] }},
-	fillColor: '{{ $options['fillColor'] }}',
-	fillOpacity: {{ $options['fillOpacity'] }},
-	editable: {{ $options['editable'] ? 'true' : 'false' }}
+var polygon_{!! $id !!} = new google.maps.Polygon({
+	paths: polygonCoordinates_{!! $id !!},
+	strokeColor: '{!! $options['strokeColor'] !!}',
+	strokeOpacity: {!! $options['strokeOpacity'] !!},
+	strokeWeight: {!! $options['strokeWeight'] !!},
+	fillColor: '{!! $options['fillColor'] !!}',
+	fillOpacity: {!! $options['fillOpacity'] !!},
+	editable: {!! $options['editable'] ? 'true' : 'false' !!}
 });
 
-polygon_{{ $id }}.setMap({{ $options['map'] }});
+polygon_{!! $id !!}.setMap({!! $options['map'] !!});
