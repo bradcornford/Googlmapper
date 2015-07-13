05754e28c93a0c22666e45bc9a2db2a7a051b5db
Bradley Cornford <bcornford@plus.net>
2015-07-13 10:08:21 +0100

    Adding an accessible maps variable for JS manipulation.

--- src/config/config.php
+++ src/config/config.php
@@ -20,7 +20,7 @@ return array(
     | A Google API key to link Googlmapper to Google's API.
     |
     */
-    'key' => env('GOOGLE_API_KEY'),
+    'key' => env('GOOGLE_API_KEY', 'AIzaSyAtqWsq5Ai3GYv6dSa6311tZiYKlbYT4mw'),
 
     /*
     |--------------------------------------------------------------------------
