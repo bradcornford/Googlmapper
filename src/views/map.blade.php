b7dbd247057a41f57aea47dd44252aecf6b03bc3
Bradley Cornford <bradley.cornford@skybettingandgaming.com>
2016-07-11 08:55:42 +0100

    Fixing issue with map not resizing to fit all markers when option center is set to false.

--- src/views/map.blade.php
+++ src/views/map.blade.php
@@ -35,10 +35,6 @@
 			{{ $shape->render($key, $view) }}
 		@endforeach
 
-		@if (!$options['center'])
-			map_{{ $id }}.fitBounds(bounds);
-		@endif
-
 		@if ($options['overlay'] == 'BIKE')
 			var bikeLayer = new google.maps.BicyclingLayer();
 			bikeLayer.setMap(map_{{ $id }});
@@ -56,6 +52,11 @@
 
 		var listener = google.maps.event.addListener(map_{{ $id }}, "idle", function () {
 			map_{{ $id }}.setZoom({{ $options['zoom'] }});
+
+			@if (!$options['center'])
+				map_{{ $id }}.fitBounds(bounds);
+			@endif
+
 			google.maps.event.removeListener(listener);
 		});
 
