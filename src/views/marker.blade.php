126205467b9efdb668d8186af2fcc34078e71472
Bradley Cornford <bradley.cornford@skybettingandgaming.com>
2017-09-12 15:18:25 +0100

    Allow marker labels as an array.

--- src/views/marker.blade.php
+++ src/views/marker.blade.php
@@ -29,7 +29,7 @@ var marker_{{ $id }} = new google.maps.Marker({
 	@endif
 
 	title: {{ json_encode((string) $options['title']) }},
-	label: {{ json_encode((string) $options['label']) }},
+	label: {{ json_encode($options['label']) }},
 	animation: @if (empty($options['animation']) || $options['animation'] == 'NONE') '' @else google.maps.Animation.{{ $options['animation'] }} @endif,
 
 	@if (isset($options['draggable']) && $options['draggable'] == true)
